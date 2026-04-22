f1451a2a1c006b66db08cf02a1d8062f927cb883
RemyDuijkeren <remy@automatevalue.com>
2026-04-22 15:04:10 +0800

    Remove `Flowline.SourceGenerators` project and related files, dependencies, and references from the solution.

--- src/Flowline.SourceGenerators/CustomApiMappingGenerator.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.CodeAnalysis;
-
-namespace Flowline.SourceGenerators;
-
-[Generator]
-public class CustomApiMappingGenerator : IIncrementalGenerator
-{
-    public void Initialize(IncrementalGeneratorInitializationContext context) { }
-}
